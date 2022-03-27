24bb087a4a0b214cc0e33b54eb2441ac1b0e85fe
Kageshiro <odest66@gmail.com>
2022-03-28 00:40:25 +0200

    Изначальная настройка проекта
    Добавил пару пакетов которые точно будут нужны, удалил всёё лишнее и поменял пару настроек

---
+++ Assets/Scripts/MarchingCube.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ManExe
+{
+    public class NewBehaviourScript : MonoBehaviour
+    {
+
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+        
+        }
+    }
+}
