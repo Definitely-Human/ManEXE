deedd5dc1c86413f63304c35fdce94354133a571
Kageshiro <odest66@gmail.com>
2022-04-03 17:28:11 +0200

    Terrain generation with marching cubes, Smooth terrain, Triplanar texturing using unity shader graph

--- Assets/Scripts/MarchingCube.cs
+++ Assets/Scripts/MarchingCube.cs
@@ -4,29 +4,113 @@ using UnityEngine;
 
 namespace ManExe
 {
-    public class MarchingCube : MonoBehaviour
-    {
+	public class MarchingCube : MonoBehaviour
+	{
+
+		public bool smoothTerrain;
+		public bool flatShaded;
+
+		MeshFilter meshFilter;
+		MeshCollider meshCollider;
+
+		float terrainSurface = 0.5f;
+		int width = 32;
+		int height = 8;
+		float[,,] terrainMap;
+
 		List<Vector3> vertices = new List<Vector3>();
 		List<int> triangles = new List<int>();
 
-		MeshFilter meshFilter;
+		private void Start()
+		{
 
-		int _configIndex;
+			meshFilter = GetComponent<MeshFilter>();
+			meshCollider = GetComponent<MeshCollider>();
+			transform.tag = "Terrain";
+			terrainMap = new float[width + 1, height + 1, width + 1];
+			PopulateTerrainMap();
+			CreateMeshData();
 
-        private void Update()
-        {
-            
-        }
+		}
 
-        void MarchCube(Vector3 position, int configIndex)
-        {
+		void CreateMeshData()
+		{
+
+			ClearMeshData();
+
+			// Loop through each "cube" in our terrain.
+			for (int x = 0; x < width; x++)
+			{
+				for (int y = 0; y < height; y++)
+				{
+					for (int z = 0; z < width; z++)
+					{
+
+						// Pass the value into our MarchCube function.
+						MarchCube(new Vector3Int(x, y, z));
+
+					}
+				}
+			}
+
+			BuildMesh();
+
+		}
+
+		void PopulateTerrainMap()
+		{
+
+			// The data points for terrain are stored at the corners of our "cubes", so the terrainMap needs to be 1 larger
+			// than the width/height of our mesh.
+			for (int x = 0; x < width + 1; x++)
+			{
+				for (int z = 0; z < width + 1; z++)
+				{
+					for (int y = 0; y < height + 1; y++)
+					{
+						float thisHeight;
+						if (x > 5 && x < 15 && y > 1 && y < 15 && z > 5 && z < 15)
+						{
+							thisHeight = 1;
+						}
+						else
+							// Get a terrain height using regular old Perlin noise.
+							thisHeight = (float)height * Mathf.PerlinNoise((float)x / 16f * 1.5f + 0.001f, (float)z / 16f * 1.5f + 0.001f);
+
+						// Set the value of this point in the terrainMap.
+						terrainMap[x, y, z] = (float)y - thisHeight;
+
+					}
+				}
+			}
+		}
+
+		void MarchCube(Vector3Int position)
+		{
+
+			// Sample terrain values at each corner of the cube.
+			float[] cube = new float[8];
+			for (int i = 0; i < 8; i++)
+			{
+
+				cube[i] = SampleTerrain(position + CornerTable[i]);
+
+			}
+
+			// Get the configuration index of this cube.
+			int configIndex = GetCubeConfiguration(cube);
+
+			// If the configuration of this cube is 0 or 255 (completely inside the terrain or completely outside of it) we don't need to do anything.
 			if (configIndex == 0 || configIndex == 255)
 				return;
+
+			// Loop through the triangles. There are never more than 5 triangles to a cube and only three vertices to a triangle.
 			int edgeIndex = 0;
-			for(int tri = 0; tri < 5; tri++)// Loop through every triangle, never more than 5
+			for (int i = 0; i < 5; i++)
 			{
-				for(int p = 0; p < 3; p++)// Loop through every point of a triangle
+				for (int p = 0; p < 3; p++)
 				{
+
 					// Get the current indice. We increment triangleIndex through each loop.
 					int indice = TriangleTable[configIndex, edgeIndex];
 
@@ -35,40 +119,138 @@ namespace ManExe
 						return;
 
 					// Get the vertices for the start and end of this edge.
-					Vector3 vert1 = position + EdgeTable[indice, 0];
-					Vector3 vert2 = position + EdgeTable[indice, 1];
+					Vector3 vert1 = position + CornerTable[EdgeIndexes[indice, 0]];
+					Vector3 vert2 = position + CornerTable[EdgeIndexes[indice, 1]];
 
-					// Get the midpoint of this edge.
-					Vector3 vertPosition = (vert1 + vert2) / 2f;
+					Vector3 vertPosition;
+					if (smoothTerrain)
+					{
+
+						// Get the terrain values at either end of our current edge from the cube array created above.
+						float vert1Sample = cube[EdgeIndexes[indice, 0]];
+						float vert2Sample = cube[EdgeIndexes[indice, 1]];
+
+						// Calculate the difference between the terrain values.
+						float difference = vert2Sample - vert1Sample;
+
+						// If the difference is 0, then the terrain passes through the middle.
+						if (difference == 0)
+							difference = terrainSurface;
+						else
+							difference = (terrainSurface - vert1Sample) / difference;
+
+						// Calculate the point along the edge that passes through.
+						vertPosition = vert1 + ((vert2 - vert1) * difference);
+
+
+					}
+					else
+					{
+
+						// Get the midpoint of this edge.
+						vertPosition = (vert1 + vert2) / 2f;
+
+					}
 
 					// Add to our vertices and triangles list and incremement the edgeIndex.
-					vertices.Add(vertPosition);
-					triangles.Add(vertices.Count - 1);
+					if (flatShaded)
+					{
+
+						vertices.Add(vertPosition);
+						triangles.Add(vertices.Count - 1);
+
+					}
+					else
+						triangles.Add(VertForIndice(vertPosition));
+
 					edgeIndex++;
+
 				}
-            }
+			}
+		}
 
-        }
+		int GetCubeConfiguration(float[] cube)
+		{
 
-        private void Start()
-        {
-			meshFilter = GetComponent<MeshFilter>();
-        }
+			// Starting with a configuration of zero, loop through each point in the cube and check if it is below the terrain surface.
+			int configurationIndex = 0;
+			for (int i = 0; i < 8; i++)
+			{
 
-        void ClearMeshData()
-        {
+				// If it is, use bit-magic to the set the corresponding bit to 1. So if only the 3rd point in the cube was below
+				// the surface, the bit would look like 00100000, which represents the integer value 32.
+				if (cube[i] > terrainSurface)
+					configurationIndex |= 1 << i;
+
+			}
+
+			return configurationIndex;
+
+		}
+
+		public void PlaceTerrain(Vector3 pos)
+		{
+
+			Vector3Int v3Int = new Vector3Int(Mathf.CeilToInt(pos.x), Mathf.CeilToInt(pos.y), Mathf.CeilToInt(pos.z));
+			terrainMap[v3Int.x, v3Int.y, v3Int.z] = 0f;
+			CreateMeshData();
+
+		}
+
+		public void RemoveTerrain(Vector3 pos)
+		{
+
+			Vector3Int v3Int = new Vector3Int(Mathf.FloorToInt(pos.x), Mathf.FloorToInt(pos.y), Mathf.FloorToInt(pos.z));
+			terrainMap[v3Int.x, v3Int.y, v3Int.z] = 1f;
+			CreateMeshData();
+
+		}
+
+		float SampleTerrain(Vector3Int point)
+		{
+
+			return terrainMap[point.x, point.y, point.z];
+
+		}
+
+		int VertForIndice(Vector3 vert)
+		{
+
+			// Loop through all the vertices currently in the vertices list.
+			for (int i = 0; i < vertices.Count; i++)
+			{
+
+				// If we find a vert that matches ours, then simply return this index.
+				if (vertices[i] == vert)
+					return i;
+
+			}
+
+			// If we didn't find a match, add this vert to the list and return last index.
+			vertices.Add(vert);
+			return vertices.Count - 1;
+
+		}
+
+		void ClearMeshData()
+		{
+
 			vertices.Clear();
 			triangles.Clear();
-        }
+
+		}
 
 		void BuildMesh()
-        {
+		{
+
 			Mesh mesh = new Mesh();
 			mesh.vertices = vertices.ToArray();
 			mesh.triangles = triangles.ToArray();
 			mesh.RecalculateNormals();
 			meshFilter.mesh = mesh;
-        }
+			meshCollider.sharedMesh = mesh;
+
+		}
 
 		Vector3Int[] CornerTable = new Vector3Int[8] {
 
@@ -83,20 +265,9 @@ namespace ManExe
 
 	};
 
-		Vector3[,] EdgeTable = new Vector3[12, 2] {
+		int[,] EdgeIndexes = new int[12, 2] {
 
-		{ new Vector3(0.0f, 0.0f, 0.0f), new Vector3(1.0f, 0.0f, 0.0f) },
-		{ new Vector3(1.0f, 0.0f, 0.0f), new Vector3(1.0f, 1.0f, 0.0f) },
-		{ new Vector3(0.0f, 1.0f, 0.0f), new Vector3(1.0f, 1.0f, 0.0f) },
-		{ new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 1.0f, 0.0f) },
-		{ new Vector3(0.0f, 0.0f, 1.0f), new Vector3(1.0f, 0.0f, 1.0f) },
-		{ new Vector3(1.0f, 0.0f, 1.0f), new Vector3(1.0f, 1.0f, 1.0f) },
-		{ new Vector3(0.0f, 1.0f, 1.0f), new Vector3(1.0f, 1.0f, 1.0f) },
-		{ new Vector3(0.0f, 0.0f, 1.0f), new Vector3(0.0f, 1.0f, 1.0f) },
-		{ new Vector3(0.0f, 0.0f, 0.0f), new Vector3(0.0f, 0.0f, 1.0f) },
-		{ new Vector3(1.0f, 0.0f, 0.0f), new Vector3(1.0f, 0.0f, 1.0f) },
-		{ new Vector3(1.0f, 1.0f, 0.0f), new Vector3(1.0f, 1.0f, 1.0f) },
-		{ new Vector3(0.0f, 1.0f, 0.0f), new Vector3(0.0f, 1.0f, 1.0f) }
+		{0, 1}, {1, 2}, {3, 2}, {0, 3}, {4, 5}, {5, 6}, {7, 6}, {4, 7}, {0, 4}, {1, 5}, {2, 6}, {3, 7}
 
 	};
 
@@ -360,5 +531,6 @@ namespace ManExe
 		{-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1}
 
 	};
+
 	}
 }
--- Assets/Scripts/SimpleCameraController.cs
+++ Assets/Scripts/SimpleCameraController.cs
@@ -8,6 +8,7 @@ namespace UnityTemplateProjects
 {
     public class SimpleCameraController : MonoBehaviour
     {
+        public Camera cam;
         class CameraState
         {
             public float yaw;
@@ -163,7 +164,7 @@ namespace UnityTemplateProjects
 #endif
             return direction;
         }
-        
+
         void Update()
         {
             // Exit Sample  
@@ -171,9 +172,9 @@ namespace UnityTemplateProjects
             if (IsEscapePressed())
             {
                 Application.Quit();
-				#if UNITY_EDITOR
-				UnityEditor.EditorApplication.isPlaying = false; 
-				#endif
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#endif
             }
 
             // Hide and lock cursor when right mouse button pressed
@@ -195,13 +196,13 @@ namespace UnityTemplateProjects
                 var mouseMovement = GetInputLookRotation() * k_MouseSensitivityMultiplier * mouseSensitivity;
                 if (invertY)
                     mouseMovement.y = -mouseMovement.y;
-                
+
                 var mouseSensitivityFactor = mouseSensitivityCurve.Evaluate(mouseMovement.magnitude);
 
                 m_TargetCameraState.yaw += mouseMovement.x * mouseSensitivityFactor;
                 m_TargetCameraState.pitch += mouseMovement.y * mouseSensitivityFactor;
             }
-            
+
             // Translation
             var translation = GetInputTranslationDirection() * Time.deltaTime;
 
@@ -210,7 +211,7 @@ namespace UnityTemplateProjects
             {
                 translation *= 10.0f;
             }
-            
+
             // Modify movement by a boost factor (defined in Inspector and modified in play mode through the mouse scroll wheel)
             boost += GetBoostFactor();
             translation *= Mathf.Pow(2.0f, boost);
@@ -224,6 +225,7 @@ namespace UnityTemplateProjects
             m_InterpolatingCameraState.LerpTowards(m_TargetCameraState, positionLerpPct, rotationLerpPct);
 
             m_InterpolatingCameraState.UpdateTransform(transform);
+
         }
 
         float GetBoostFactor()
