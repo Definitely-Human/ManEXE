4b3ad421abff19fd33f319560c8b414c7d7d9334
Kageshiro <odest66@gmail.com>
2022-05-22 23:04:41 +0200

    Support for multiple types of Voxel
    - Added support for multiple types of Voxel  in the terrain
    - Ability to place and destroy voxels

---
+++ Assets/Scripts/Scriptable Objects/TerrainType.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ManExe
+{
+    [CreateAssetMenu(fileName = "New TerrainType", menuName = "Scriptable/TerrainType", order = 0)]
+    public class TerrainType : ScriptableObject
+    {
+        // ===== Data =====
+        [SerializeField]
+        private int _terreinTypeID;
+        [SerializeField]
+        private string _name;
+
+        // ===== Texture =====
+        [SerializeField]
+        private int _topTexID;
+        [SerializeField]
+        private int _sideTexID;
+
+        public int TerreinTypeID { get => _terreinTypeID; set => _terreinTypeID = value; }
+        public string Name { get => _name; set => _name = value; }
+        public int TopTexID { get => _topTexID; set => _topTexID = value; }
+        public int SideTexID { get => _sideTexID; set => _sideTexID = value; }
+    }
+}
